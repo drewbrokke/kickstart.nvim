fffd85437a3e1748339b17f289b3cab562052640
Drew Brokke <drew.brokke@liferay.com>
2025-06-30 13:23:35 -0500

    init.lua - try my own key combos for searching

--- init.lua
+++ init.lua
@@ -449,6 +449,15 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      -- Drew
+
+      vim.keymap.set('n', '<leader>sG', function()
+        builtin.find_files {
+          find_command = { 'git', 'ls-files' },
+        }
+      end, { desc = '[S]earch [G]iles with Git' })
+      vim.keymap.set('n', '<leader>s"', builtin.registers, { desc = "[S]earch Registers ('\"' for registers)" })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
